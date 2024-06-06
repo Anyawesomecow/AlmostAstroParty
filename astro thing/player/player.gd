9774f2e0668a713a1575b0a724a2ed1952882093
Zeepnely <kamheater.55@gmail.com>
2024-06-05 18:32:45 -0700

    Signed-off-by: Zeepnely <kamheater.55@gmail.com>

--- astro thing/player/player.gd
+++ astro thing/player/player.gd
@@ -1,11 +1,21 @@
 extends Node2D
 var velocity = Vector2(0, 0)
-var angle = 0
+var speed = 200
+var angle = 0.0
+var turnDirection = true
+var turnConstant = PI/45
+@onready var player = $"."
+@onready var sprite = $Sprite2D
+#var turn = 0.0
 
-func turn():
-	pass
-	
+func _input(event):
+	if event.is_action_pressed("esc"):
+		get_tree().quit()
+
 func _physics_process(delta):
-	
-	velocity = Vector2(9, 9)
+	if Input.is_action_pressed("turn"):
+		angle += turnConstant * (1 if turnDirection else -1)
+		
 	position += velocity * delta
+	velocity.x = lerp(velocity.x, cos(angle) * speed, delta)
+	velocity.y = lerp(velocity.y, sin(angle) * speed, delta)
