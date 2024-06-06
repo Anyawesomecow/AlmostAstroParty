15d8310602c34f04f654b84a4888dbe6821b192f
Anyawesomecow <isaacljoshi@gmail.com>
2024-06-06 13:02:49 -0700

    scween wap

--- astro thing/player/player.gd
+++ astro thing/player/player.gd
@@ -3,6 +3,7 @@ var velocity = Vector2(0, 0)
 var speed = 200
 var turnDirection = 1
 var turnConstant = PI
+@onready var SIZE = get_viewport_rect().size
 @onready var sprite = $Sprite2D
 #var turn = 0.0
 func _input(event):
@@ -14,4 +15,5 @@ func _physics_process(delta):
 		rotation += turnConstant * turnDirection * delta
 		
 	position += velocity * delta
+	position = Vector2(wrapf(position.x, 0, SIZE.x), wrapf(position.y, 0, SIZE.y))
 	velocity = Vector2(lerp(velocity.x, cos(rotation) * speed, delta * .6), lerp(velocity.y, sin(rotation) * speed, delta * .6))
