4232557e9e9f8132826fef1cfaea72002a158a2d
Anyawesomecow <isaacljoshi@gmail.com>
2024-06-05 19:15:39 -0700

    made the game better because im built different

--- astro thing/player/player.gd
+++ astro thing/player/player.gd
@@ -3,18 +3,16 @@ var velocity = Vector2(0, 0)
 var speed = 200
 var angle = 0.0
 var turnDirection = 1
-var turnConstant = PI/45
+var turnConstant = PI
 @onready var sprite = $Sprite2D
 #var turn = 0.0
-
 func _input(event):
 	if event.is_action_pressed("esc"):
 		get_tree().quit()
-
+		
 func _physics_process(delta):
 	if Input.is_action_pressed("turn"):
-		angle += turnConstant * turnDirection
+		rotation += turnConstant * turnDirection * delta
 		
 	position += velocity * delta
-	velocity.x = lerp(velocity.x, cos(angle) * speed, delta)
-	velocity.y = lerp(velocity.y, sin(angle) * speed, delta)
+	velocity = Vector2(lerp(velocity.x, cos(rotation) * speed, delta), lerp(velocity.y, sin(rotation) * speed, delta))
