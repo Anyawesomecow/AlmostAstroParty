1f2fc4a273b6befc8e40487b24407d326fc28cfe
Zeepnely <kamheater.55@gmail.com>
2024-06-06 21:53:15 -0700

    Signed-off-by: Zeepnely <kamheater.55@gmail.com>
    added more to main menue(no actual funcionality)

--- astro thing/UI/Menus/Main Menue.gd
+++ astro thing/UI/Menus/Main Menue.gd
@@ -1,9 +1,14 @@
 extends Control
-
+@onready var startButton = $VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/Start
+@onready var joinButton = $VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/Join
+@onready var settingsButton = $VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/settings
+@onready var hostButton = $VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/host
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	joinButton.hide()
+	settingsButton.hide()
+	hostButton.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +17,19 @@ func _process(delta):
 
 # starts game
 func _on_start_pressed():
+	startButton.hide()
+	joinButton.show()
+	settingsButton.show()
+	hostButton.show()
+
+
+func _on_join_pressed():
 	get_tree().change_scene_to_file("res://leval.tscn")
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_host_pressed():
+	pass # Replace with function body.
