7303093f1018eec6720b498ad29eb49e6d497e5e
Zeepnely <kamheater.55@gmail.com>
2024-06-07 20:01:39 -0700

    did stuff probably idfk
    Signed-off-by: Zeepnely <kamheater.55@gmail.com>

--- astro thing/player/player.gd
+++ astro thing/player/player.gd
@@ -1,19 +1,40 @@
-extends Node2D
-var velocity = Vector2(0, 0)
+extends CharacterBody2D
+
 var speed = 200
 var turnDirection = 1
 var turnConstant = PI
-@onready var SIZE = get_viewport_rect().size
+@onready var SIZE = get_viewport_rect().size + Vector2(100, 100)
 @onready var sprite = $Sprite2D
-#var turn = 0.0
+@onready var dashTimer = $dashTimer
+@onready var dashCooldown = $dashCooldown
+
+func dash():
+	rotation += PI/2
+	velocity = Vector2(150 * cos(rotation), 150 * sin(rotation))
+	dashCooldown.start()
+
 func _input(event):
 	if event.is_action_pressed("esc"):
 		get_tree().quit()
 		
 func _physics_process(delta):
+	
+	if Input.is_action_just_pressed("turn"):
+		print(dashTimer.time_left)
+		if dashTimer.time_left > 0 and dashCooldown.time_left == 0:
+			dash()
+		else:
+			dashTimer.start()
+	
+	var collisionInfo = move_and_collide(velocity * delta)
+	
 	if Input.is_action_pressed("turn"):
 		rotation += turnConstant * turnDirection * delta
+		pass
+	
+	if collisionInfo:
+		velocity = velocity.slide(collisionInfo.get_normal())
 		
-	position += velocity * delta
+	print(velocity)
 	position = Vector2(wrapf(position.x, 0, SIZE.x), wrapf(position.y, 0, SIZE.y))
 	velocity = Vector2(lerp(velocity.x, cos(rotation) * speed, delta * .6), lerp(velocity.y, sin(rotation) * speed, delta * .6))
