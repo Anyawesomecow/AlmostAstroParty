b9189b6381021679224416c5aebbb1e5221acff8
Zeepnely <kamheater.55@gmail.com>
2024-06-09 15:34:06 -0700

    Signed-off-by: Zeepnely <kamheater.55@gmail.com>
    added bullet screenwrap

--- astro thing/player/bullet.gd
+++ astro thing/player/bullet.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var SIZE = get_viewport_rect().size + Vector2(700, 700)
 var velocity = Vector2()
 var me = "res://player/bullet.tscn"
 const SPEED = 300.0
@@ -8,6 +9,7 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
+	position = Vector2(wrapf(position.x, 0, SIZE.x), wrapf(position.y, 0, SIZE.y))
 	position += velocity * delta
 	velocity = Vector2(cos(rotation) * 20000 * delta, sin(rotation) * 20000 * delta)
 
