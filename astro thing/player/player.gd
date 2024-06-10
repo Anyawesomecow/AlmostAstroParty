9a33d6a381644653aebfdedbaf815553432eafa8
Anyawesomecow <isaacljoshi@gmail.com>
2024-06-09 19:09:27 -0700

    fixed evil code

--- astro thing/player/player.gd
+++ astro thing/player/player.gd
@@ -30,13 +30,6 @@ func _ready():
 	shipParticals3.amount = defaultBurtAmountBurst
 	shipParticals4.amount = defaultBurtAmountBurst
 
-func getsHit():
-	if is_noodle == true:
-		queue_free()
-	else:
-		$denoodling.start()
-		is_noodle = true
-
 func dash():
 	boostVisuals()
 	rotation = angleBefore + PI/2.5
@@ -49,9 +42,38 @@ func boostVisuals():
 	shipParticals3.emitting = true
 	shipParticals4.emitting = true
 
+func _on_amorecharge_timeout():
+	if amo < 3:
+		amo += 1
+
+func _on_denoodling_timeout():
+	$ship.show()
+	$noodle.hide()
+	
+	shipParticals1.emitting = true
+	shipParticals2.emitting = true
+
+	is_noodle = false
+
+func _on_area_2d_area_entered(area):
+	if is_noodle == true:
+		queue_free()
+	else:
+		$denoodling.start()
+		is_noodle = true
+	shipParticals1.emitting = false
+	shipParticals2.emitting = false
+	$ship.hide()
+	$noodle.show()
+
+func _on_dash_cooldown_timeout():
+	shipParticals1.emitting = true
+	shipParticals2.emitting = true
+	shipParticals3.emitting = false
+	shipParticals4.emitting = false
+
 func _input(event):
 	if event.is_action_pressed("shoot") and amo > 0 and is_noodle == false:
-		print("florp")
 		Events.emit_signal("shooting")
 		amo -= 1
 	
@@ -59,19 +81,7 @@ func _input(event):
 		get_tree().quit()
 		
 func _physics_process(delta):
-	
-	
-	
-	if is_noodle == true:
-		shipParticals1.emitting = false
-		shipParticals2.emitting = false
-		$ship.hide()
-		$noodle.show()
-	else:
-		shipParticals1.emitting = true
-		shipParticals2.emitting = true
-	
-	
+	var collisionInfo = move_and_collide(velocity * delta)
 	
 	if Input.is_action_just_pressed("turn"):
 		if dashTimer.time_left > 0 and dashCooldown.time_left == 0 and is_noodle == false:
@@ -79,8 +89,6 @@ func _physics_process(delta):
 		else:
 			angleBefore = rotation
 			dashTimer.start()
-	
-	var collisionInfo = move_and_collide(velocity * delta)
 	
 	if Input.is_action_pressed("turn"):
 		rotation += turnConstant * turnDirection * delta
@@ -101,28 +109,3 @@ func _physics_process(delta):
 		velocity.y = lerp(velocity.y, float(0), delta * .8)
 		
 	position = Vector2(wrapf(position.x, 0, SIZE.x), wrapf(position.y, 0, SIZE.y))
-
-func _on_amorecharge_timeout():
-	if amo < 3:
-		amo += 1
-
-
-func _on_denoodling_timeout():
-	$ship.show()
-	$noodle.hide()
-	
-	is_noodle = false
-
-
-func _on_area_2d_area_entered(area):
-	getsHit()
-
-
-
-
-
-func _on_dash_cooldown_timeout():
-	shipParticals1.emitting = true
-	shipParticals2.emitting = true
-	shipParticals3.emitting = false
-	shipParticals4.emitting = false
