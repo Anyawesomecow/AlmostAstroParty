0faa144597ca629678dca32d33db02393f2ffa11
Zeepnely <kamheater.55@gmail.com>
2024-06-09 18:43:51 -0700

    Signed-off-by: Zeepnely <kamheater.55@gmail.com>
    added noodle things

--- astro thing/player/player.gd
+++ astro thing/player/player.gd
@@ -6,6 +6,12 @@ var speed = 200
 var turnDirection = 1
 var turnConstant = PI
 var amo = 3
+var defaultBurtAmount = 50
+var defaultBurtAmountBurst = 100
+@onready var shipParticals1 = $particals_ship/CPUParticles2D
+@onready var shipParticals2 = $particals_ship/CPUParticles2D2
+@onready var shipParticals3 = $particals_ship/CPUParticles2D3
+@onready var shipParticals4 = $particals_ship/CPUParticles2D4
 @onready var SIZE = Vector2(700, 400)
 @onready var sprite = $Sprite2D
 @onready var dashTimer = $dashTimer
@@ -15,7 +21,14 @@ var amo = 3
 func _ready():
 	$noodle.hide()
 	$ship.show()
-
+	shipParticals1.emitting = true
+	shipParticals2.emitting = true
+	shipParticals3.emitting = false
+	shipParticals4.emitting = false
+	shipParticals1.amount = defaultBurtAmount
+	shipParticals2.amount = defaultBurtAmount
+	shipParticals3.amount = defaultBurtAmountBurst
+	shipParticals4.amount = defaultBurtAmountBurst
 
 func getsHit():
 	if is_noodle == true:
@@ -25,12 +38,19 @@ func getsHit():
 		is_noodle = true
 
 func dash():
+	boostVisuals()
 	rotation = angleBefore + PI/2.5
 	velocity = Vector2(150 * cos(rotation), 150 * sin(rotation))
 	dashCooldown.start()
 
+func boostVisuals():
+	shipParticals1.emitting = false
+	shipParticals2.emitting = false
+	shipParticals3.emitting = true
+	shipParticals4.emitting = true
+
 func _input(event):
-	if event.is_action_pressed("shoot") and amo > 0:
+	if event.is_action_pressed("shoot") and amo > 0 and is_noodle == false:
 		print("florp")
 		Events.emit_signal("shooting")
 		amo -= 1
@@ -40,13 +60,21 @@ func _input(event):
 		
 func _physics_process(delta):
 	
+	
+	
 	if is_noodle == true:
-		
+		shipParticals1.emitting = false
+		shipParticals2.emitting = false
 		$ship.hide()
 		$noodle.show()
+	else:
+		shipParticals1.emitting = true
+		shipParticals2.emitting = true
+	
+	
 	
 	if Input.is_action_just_pressed("turn"):
-		if dashTimer.time_left > 0 and dashCooldown.time_left == 0:
+		if dashTimer.time_left > 0 and dashCooldown.time_left == 0 and is_noodle == false:
 			dash()
 		else:
 			angleBefore = rotation
@@ -60,9 +88,19 @@ func _physics_process(delta):
 	if collisionInfo:
 		velocity = velocity.slide(collisionInfo.get_normal())
 		
+	if is_noodle == false:
+			velocity = Vector2(lerp(velocity.x, cos(rotation) * speed, delta * .6), lerp(velocity.y, sin(rotation) * speed, delta * .6))
+	else:
+		if Input.is_action_pressed("shoot"):
+			velocity = Vector2(lerp(velocity.x, cos(rotation) * speed, delta * .6), lerp(velocity.y, sin(rotation) * speed, delta * .6))
+			
+	if velocity.x > 0 or velocity.x < 0:
+		velocity.x = lerp(velocity.x, float(0), delta * .8)
+		
+	if velocity.y > 0 or velocity.y < 0:
+		velocity.y = lerp(velocity.y, float(0), delta * .8)
+		
 	position = Vector2(wrapf(position.x, 0, SIZE.x), wrapf(position.y, 0, SIZE.y))
-	velocity = Vector2(lerp(velocity.x, cos(rotation) * speed, delta * .6), lerp(velocity.y, sin(rotation) * speed, delta * .6))
-
 
 func _on_amorecharge_timeout():
 	if amo < 3:
@@ -72,8 +110,19 @@ func _on_amorecharge_timeout():
 func _on_denoodling_timeout():
 	$ship.show()
 	$noodle.hide()
+	
 	is_noodle = false
 
 
 func _on_area_2d_area_entered(area):
 	getsHit()
+
+
+
+
+
+func _on_dash_cooldown_timeout():
+	shipParticals1.emitting = true
+	shipParticals2.emitting = true
+	shipParticals3.emitting = false
+	shipParticals4.emitting = false
